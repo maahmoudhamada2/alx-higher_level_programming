2f3b15aa0d492c242c4bfe0379068e639b25bf0b
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-07-30 22:08:02 +0300

    Re-submit: 0x0F-python-object_relational_mapping -> 8-model_state_fetch_first.py

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""script that lists all State objects from the database hbtn_0e_6_usa"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == '__main__':
+    path = "mysql+mysqldb://{}:{}@localhost:3306/{}"\
+        .format(argv[1], argv[2], argv[3])
+    engine = create_engine(path)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    row = session.query(State.id, State.name).first()
+    print("{}: {}".format(row.id, row.name))
