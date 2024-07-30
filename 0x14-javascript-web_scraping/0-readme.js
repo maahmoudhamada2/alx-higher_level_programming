b47b8cf66b675f990180cecea5b4ed40a08558a4
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-07-30 13:21:44 +0300

    Submit: 0x14-javascript-web_scraping --> 0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const args = process.argv.slice(2);
+const filePath = args[0];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+}
+);
