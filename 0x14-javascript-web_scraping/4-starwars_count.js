268f85cdb54f8657ff4c5e1ffa3f11114cc77a94
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-07-30 15:58:40 +0300

    Fixing: 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,26 +1,12 @@
 #!/usr/bin/node
-
 const request = require('request');
-const args = process.argv.slice(2);
-const url = args[0];
-const charUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
-let count = 0;
-
-request(url, (err, response, body) => {
-  if (err) {
-    console.error(err);
-    return;
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
-  const myJson = JSON.parse(body);
-  const result = myJson.results;
-  for (let i = 0; i < result.length; i++) {
-    const chars = result[i].characters;
-    for (let j = 0; j < chars.length; j++) {
-      if (chars[j] === charUrl) {
-        count++;
-      }
-    }
-  }
-  console.log(count);
-}
-);
+});
