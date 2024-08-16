fb1a92e769011f035c7d64e0c968beb344c3c2ba
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-08-16 18:54:12 +0300

    Deleting: 0x12-javascript-warm_up

--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-
-const args = process.argv.slice(2);
-const output = args[0] + ' is ' + args[1];
-console.log(output);
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const progLang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let i = 0; i < progLang.length; i++) {
-  console.log(progLang[i]);
-}
