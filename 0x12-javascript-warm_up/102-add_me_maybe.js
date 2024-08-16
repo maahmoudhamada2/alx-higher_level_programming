68ff349d06337989f42b6e88b780be6a0f188b95
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-08-16 22:47:57 +0300

    Submit: 0x12-javascript-warm_up --> 102-add_me_maybe.js

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Function that increments and calls a function.
+
+exports.addMeMaybe = function (number, theFunction) {
+  let count = 1;
+  for (let x = 0; x < number; x++) {
+    count++;
+  }
+
+  theFunction(count);
+};
