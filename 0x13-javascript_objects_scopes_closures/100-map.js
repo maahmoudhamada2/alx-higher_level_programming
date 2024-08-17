8e7a9302bbb3fd9cd29ae18da87d04608c60a66e
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-08-17 15:10:57 +0300

    Deleting: 0x13-javascript_objects....

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-const list = require('./100-data.js').list;
-console.log(list);
-const a = list.map(function (value, index) {
-  return value * index;
-});
-console.log(a);
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-exports.esrever = function (list) {
-  const revsVersion = [];
-  for (let i = list.length - 1; i >= 0; i--) {
-    revsVersion.push(list[i]);
-  }
-  return revsVersion;
-};
