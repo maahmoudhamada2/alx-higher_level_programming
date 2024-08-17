3ecc839cbc8b956388184b033d6d59ceb35154dd
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-08-17 21:52:46 +0300

    Submit: 0x14-javascript --> 2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Script that display the status code of a GET request
+
+const request = require('request');
+const args = process.argv.slice(2);
+
+const url = args[0];
+
+request.get(url, function (err, response, body) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('Code:', response.statusCode);
+  }
+});
