77cab0a1e8ca07f9afb71e1f2bc321f70431ffa2
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-08-18 17:33:34 +0300

    Fixing: 0x13-javascript --> 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,18 +1,14 @@
 #!/usr/bin/node
 // Class Square that defines a square and inherits from Rectangle of 4-rectangle.js
 
-const Rectangle = require('./4-rectangle.js');
+const origSquare = require('./5-square.js');
 
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+module.exports = class Square extends origSquare {
   charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
-      for (let h = 0; h < this.width; h++) {
+      for (let h = 0; h < this.height; h++) {
         for (let w = 0; w < this.width; w++) {
           process.stdout.write(c);
         }
